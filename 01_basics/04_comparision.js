384d22351ff60078d5220c482f3e1fd3fe88994e
aryanvermacentral-coder <aryanvermacentral@gmail.com>
2026-09-13 21:50:26 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(3 > 2) //true
+// console.log(2 >= 1) //true
+// console.log(2 < 1); //false
+// console.log(2 == 1+1); // true
+// console.log(2 != 1); //true
+
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
+
+// console.log(null > 0); //false -> For relational comparisons (>, <, >=, <=), JavaScript converts null to a number.
+// console.log(null == 0); //false -> The == operator normally allows type conversion, but null is a special case.
+// console.log(null >= 0); //true ->
+
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+
+// === -> strict equality
+// !== -> strict inequality
+
+console.log("2" === 2); //this operator also check data type : false
+console.log();

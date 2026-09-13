6f6d797bc79234079c0599ead198d5689015abcc
aryanvermacentral-coder <aryanvermacentral@gmail.com>
2026-09-13 20:36:20 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "21"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(score);
+// console.log(typeof valueInNumber);
+
+// Notes
+// "33" -> 33
+// "33abc" -> NaN [Not A Number]
+// true -> 1 | false -> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 -> true; 0 -> false
+// "" -> false
+// "aryan" -> true
+
+let num = 33
+let strNum = String(num)
+console.log(strNum) //33
+console.log(typeof(strNum)); //string

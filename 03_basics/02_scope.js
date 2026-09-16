e28f0c55640b8db5e3843de055c5f52e8d5f2f7e
aryanvermacentral-coder <aryanvermacentral@gmail.com>
2026-09-16 19:44:49 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,19 @@
+// var c = 300
+
+let a = 100
+
+if(true) {
+    let a = 10
+    const b = 20
+    // var c = 30
+    console.log("INNER:", a);
+    
+}
+
+console.log(a); //100
+// console.log(b);
+// console.log(c); //this prints 30 because it is outside scope still it prints var c = 30
+
+
+// {} -> SCOPE
+

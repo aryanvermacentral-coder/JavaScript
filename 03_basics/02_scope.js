54c8edee09fb6a0a5f8eea7984e2ddb0e38dee48
aryanvermacentral-coder <aryanvermacentral@gmail.com>
2026-09-17 18:15:48 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -10,10 +10,51 @@ if(true) {
     
 }
 
-console.log(a); //100
+// console.log(a); //100
 // console.log(b);
 // console.log(c); //this prints 30 because it is outside scope still it prints var c = 30
 
-
 // {} -> SCOPE
 
+//nested scope
+
+function one() {
+    const username = "aryan"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website); //cannot access this
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "aryan"
+    if (username === "aryan") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+//interesting (below)
+
+console.log(addone(5))
+
+function addone(num){ //function
+    return num + 1
+}
+
+console.log(addone(5)) //can be called before and after the function
+
+const addTwo = function(num){ //function by variable holding
+    return num + 2
+}
+
+addTwo(5) //can only be called after the function not before
